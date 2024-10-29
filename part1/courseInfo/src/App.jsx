dec629cfefc76f3ec89cebf7046e6f52d00d9596
Roberto Bolanos <roberto_bolanos@icloud.com>
2024-10-29 11:03:59 -0700

    1.1 complete

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,41 +1,23 @@
-const Header = () => {
-  return (
-      <h1>
-         Half Stack application development 
-      </h1>
-  )  
-}
-
-const Content = (props) => {
-  return (
-      <p>{props.part} {}</p>
-  )  
-}
-
-const Total = (props) => {
-  return (
-      <p>Number of exercises  {props.total}</p>
-  )  
-}
+import React from 'react';
+import Header from './Components/Header';
+import Content from './Components/Content';
+import Total from './Components/Total';
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = 'Half Stack application development';
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 },
+  ];
 
   return (
     <div>
-      <Header/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
-      <Total total={exercises1 + exercises2 + exercises3}/>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
